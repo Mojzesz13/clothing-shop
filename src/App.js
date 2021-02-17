4ed9d2197cef6646cd8ad81a07812f743af1c7e3
Pawel Bozejewski <bozejewski@gmail.com>
2021-02-17 22:36:36 +0100

    updating our app to redirect to home if user is signed in

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import ShopPage from './components/pages/shop/shop';
 import HomePage from './components/pages/homepage/homepage';
+import ShopPage from './components/pages/shop/shop';
 import Header from './components/header/header';
 import SingInAndSingUp from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.action';
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -47,17 +41,27 @@ class App extends React.Component {
       <div>
         <Header />
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/hats' component={HatsPage} />
+          <Route exact path='/' component={HomePage} /> 
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SingInAndSingUp} />
+          <Route
+            exact
+            path='/signin'
+            render={() =>
+              this.props.currentUser ? <Redirect to='/' /> : <SingInAndSingUp />
+            }
+          />
         </Switch>
       </div>
     );
   }
 }
+
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser,
+});
+
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/components/pages/homepage/homepage.js
+++ src/components/pages/homepage/homepage.js
@@ -1,6 +1,11 @@
 import React from 'react';
+import Directory from '../../directory/directory';
 import './homepage.scss';
 
 export default function Homepage() {
-  return <div className='homepage'>HomePage</div>;
+  return (
+    <div className='homepage'>
+      <Directory />
+    </div>
+  );
 }
