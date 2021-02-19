ddc017a7d7dc28d783c1ee211803e2b84b0e51ee
Pawel Bozejewski <bozejewski@gmail.com>
2021-02-19 14:34:29 +0100

    updating  our application with reselect, replaced card selector with memoized selectors

--- src/components/cart-icon/card-icon.js
+++ src/components/cart-icon/card-icon.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.action';
+import { selectCartItemsCount } from '../../redux/cart/cart.selector';
 
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cart-icon.scss';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ toggleCartHidden, itemCount }) => {
   return (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shoping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{itemCount}</span>
     </div>
   );
 };
@@ -19,5 +20,8 @@ const CartIcon = ({ toggleCartHidden }) => {
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
+const mapStatetoProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStatetoProps, mapDispatchToProps)(CartIcon);
--- src/components/cart-item/cart-item.js
+++ src/components/cart-item/cart-item.js
@@ -6,7 +6,7 @@ const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
     <div className='cart-item'>
       <img src={imageUrl} alt={name} />
-      <div className='item-detalis'>
+      <div className='item-details'>
         <span className='name'>{name}</span>
         <span className='price'>
           {quantity} x ${price}
