aa27de9e2c0e7427644ab3e76dbfdc73e4056fa1
Pawel Bozejewski <bozejewski@gmail.com>
2021-02-21 08:12:22 +0100

    added removeItem and addItem functionality to ceheckout item component

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes = {
   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
   ADD_ITEM: 'ADD_ITTEM',
+  REMOVE_ITEM: "REMOVE_ITEM",
+  CLEAR_ITEM_FROM_CART: 'CLEAR_ITEM_FROM_CART',
 };
 
 export default CartActionTypes;
