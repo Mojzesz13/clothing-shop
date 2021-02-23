4e9706c8b076377f05d01dfb7ec99265140e288a
Pawel Bozejewski <bozejewski@gmail.com>
2021-02-23 14:26:34 +0100

    added directory state into redux

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -1,62 +1,25 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
 import MenuItem from '../menu-item/menu-item';
+
 import './directory.scss';
 
-class Directory extends Component {
-  constructor() {
-    super();
+const Directory = ({ sections }) => {
+  return (
+    <div className='director-menu'>
+      {sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </div>
+  );
+};
 
-    this.state = {
-      sections: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          //imageUrl: 'images/hats.png', // local file in public/images/
-          id: 1,
-          linkUrl: 'shop/hats',
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          //imageUrl: 'images/jackets.png', // local file in public/images/
-          id: 2,
-          linkUrl: 'shop/jackets',
-        },
-        {
-          title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          //imageUrl: 'images/sneakers.png', // local file in public/images/
-          id: 3,
-          linkUrl: 'shop/sneakers',
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          //imageUrl: 'images/womens.png', // local file in public/images/
-          size: 'large',
-          id: 4,
-          linkUrl: 'shop/womens',
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          //imageUrl: 'images/men.png', // local file in public/images/
-          size: 'large',
-          id: 5,
-          linkUrl: 'shop/mens',
-        },
-      ],
-    };
-  }
-  render() {
-    return (
-      <div className='director-menu'>
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
-          <MenuItem key={id} {...otherSectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const mapStatetoProps = createStructuredSelector({
+  sections: selectDirectorySections,
+});
 
-export default Directory;
+export default connect(mapStatetoProps)(Directory);
